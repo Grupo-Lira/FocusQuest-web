0a009567bf88ca0284a4a51d5cee93dd33a24f36
Amanda Oliveira <amandaol7b@gmail.com>
2025-05-05 20:19:57 -0300

    feat: update Button component to accept dynamic text and href props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,17 @@
-export const Button = () => {
+"use client";
+interface ButtonProps {
+  text: string;
+  href: string;
+}
+
+export const Button = ({ text, href }: ButtonProps) => {
     return (
       <button
+        onClick={() => window.location.href = href}
         className="
-            bg-[var(--primary)] text-[var(--white)] font-orbitron text-xl px-[4.25rem] py-2.5 rounded-xl button-glow transition-all duration-300"
+        bg-[var(--primary)] text-[var(--white)] font-orbitron text-xl px-[4.25rem] py-2.5 rounded-xl button-glow transition-all duration-300"
       >
-        Login
+        {text}
       </button>
     );
 };
