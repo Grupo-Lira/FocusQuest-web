2e0c7faea8b0efc9232909a19d85cda28b7379cf
Amanda Oliveira <amandaol7b@gmail.com>
2025-05-07 16:13:26 -0300

    feat: refactor Star component to use custom hook for mouse behavior and add StarHover component for hover effect

--- src/components/Star.tsx
+++ src/components/Star.tsx
@@ -1,7 +1,9 @@
 "use client";
-import { useRef, useState } from "react";
+
 import Image from "next/image";
 import clsx from "clsx";
+import { useStarBehavior } from "@/hooks/useStarBehavior";
+import StarHover from "./StarHover";
 
 export default function Star({
   top,
@@ -14,39 +16,10 @@ export default function Star({
   onRemove: () => void;
   onError: () => void;
 }) {
-  const [hovering, setHovering] = useState(false);
-  const [removing, setRemoving] = useState(false);
-  const [resolved, setResolved] = useState(false);
-  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
-    const enterTimeRef = useRef<number>(0);
-
-  const handleMouseEnter = () => {
-    if (resolved) return;
-    setHovering(true);
-    enterTimeRef.current = Date.now();
-    const timeout = setTimeout(() => {
-      setResolved(true);
-      setRemoving(true);
-      setTimeout(() => {
-        onRemove();
-      }, 10);
-    }, 3000); 
-
-    timeoutRef.current = timeout;
-  };
-
-  const handleMouseLeave = () => {
-    setHovering(false);
-    if (timeoutRef.current) {
-      clearTimeout(timeoutRef.current);
-      timeoutRef.current = null;
-    }
-   const timeHovered = Date.now() - enterTimeRef.current;
-   if (timeHovered >= 500 && timeHovered < 3000) {
-     // 🟥 só conta como erro se o usuário ficou pelo menos 1 segundo
-     onError();
-   }
-  };
+  const { hovering, removing, handleMouseEnter, handleMouseLeave } = useStarBehavior(
+    onRemove,
+    onError
+  );
 
   return (
     <div
@@ -58,19 +31,7 @@ export default function Star({
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
-      {hovering && (
-        <div
-          className="absolute z-0 bg-[#FFDDBD] opacity-30 rounded-full star-hover"
-          style={{
-            width: 80,
-            height: 80,
-            top: "-20px",
-            left: "-20px",
-            transform: "scale(0.2)",
-          }}
-        />
-      )}
-
+      {hovering && <StarHover />}
       <Image
         width={40}
         height={38}
