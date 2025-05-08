7f03092e57a559afe81ef98f632427f1d5029f17
Amanda Oliveira <amandaol7b@gmail.com>
2025-05-08 17:38:37 -0300

    feat: refactor GameScreen to use animatedElements array for rendering AnimatedElement components

--- src/app/fase/1/GameScreen.tsx
+++ src/app/fase/1/GameScreen.tsx
@@ -9,6 +9,46 @@ import { Bolt } from "lucide-react";
 import { useGameContext } from "@/context/GameContext";
 import { AnimatedElement } from "@/components/AnimatedElement";
 
+const animatedElements = [
+  {
+    id: "nave",
+    src: "/img/distracoes/nave.png",
+    initial: { x: "0%", y: "0%" },
+    animate: { x: "100vw", y: "-100vh" },
+    duration: 5,
+  },
+  {
+    id: "meteoro",
+    src: "/img/distracoes/meteoro.png",
+    initial: { x: "100vw", y: "-100vh" },
+    animate: { x: "0vw", y: "-30vh" },
+    duration: 3,
+  },
+  {
+    id: "meteoroet",
+    src: "/img/distracoes/meteoroet.png",
+    initial: { x: "0vw", y: "-150vh" },
+    animate: { x: "100vw", y: "-80vh" },
+    duration: 2,
+  },
+  {
+    id: "ovni",
+    src: "/img/distracoes/ovni.png",
+    initial: { x: "0vw", y: "-130vh" },
+    animate: { x: "100vw", y: "-130vh" },
+    duration: 5,
+    repeatType: "reverse" as const,
+  },
+  {
+    id: "et",
+    src: "/img/distracoes/et.png",
+    initial: { x: "30vw", y: "-130vh" },
+    animate: { x: "60vw", y: "-130vh" },
+    duration: 2,
+    repeatType: "reverse" as const,
+  },
+];
+
 export default function GameScreen() {
   const { stars, level, handleHit, handleError } = useGameLogic();
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -54,38 +94,16 @@ export default function GameScreen() {
           </div>
 
           <div className="h-screen w-screen relative">
-            <AnimatedElement
-              src="/img/distracoes/nave.png"
-              initial={{ x: "0%", y: "0%" }}
-              animate={{ x: "100vw", y: "-100vh" }}
-              duration={5}
-            />
-            <AnimatedElement
-              src="/img/distracoes/meteoro.png"
-              initial={{ x: "100vw", y: "-100vh" }}
-              animate={{ x: "0vw", y: "-30vh" }}
-              duration={3}
-            />
-            <AnimatedElement
-              src="/img/distracoes/meteoroet.png"
-              initial={{ x: "0vw", y: "-150vh" }}
-              animate={{ x: "100vw", y: "-80vh" }}
-              duration={2}
-            />
-            <AnimatedElement
-              src="/img/distracoes/ovni.png"
-              initial={{ x: "0vw", y: "-130vh" }}
-              animate={{ x: "100vw", y: "-130vh" }}
-              duration={5}
-              repeatType="reverse"
-            />
-            <AnimatedElement
-              src="/img/distracoes/et.png"
-              initial={{ x: "30vw", y: "-130vh" }}
-              animate={{ x: "60vw", y: "-130vh" }}
-              duration={2}
-              repeatType="reverse"
-            />
+            {animatedElements.map((item) => (
+              <AnimatedElement
+                key={item.id}
+                src={item.src}
+                initial={item.initial}
+                animate={item.animate}
+                duration={item.duration}
+                repeatType={item.repeatType}
+              />
+            ))}
           </div>
         </div>
       )}
