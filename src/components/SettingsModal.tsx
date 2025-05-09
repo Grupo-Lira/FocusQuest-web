aa34f8a68d8f467bb65ded76a9832da771f9ae29
Amanda Oliveira <amandaol7b@gmail.com>
2025-05-09 20:13:09 -0300

    feat: add Card component and refactor SettingsModal to utilize Card for layout

--- src/components/SettingsModal.tsx
+++ src/components/SettingsModal.tsx
@@ -1,7 +1,7 @@
-/* eslint-disable react/require-default-props */
 import Image from "next/image";
 import { useState } from "react";
 import { Button } from "./Button";
+import { Card } from "./Card"; 
 
 interface SettingPageProps {
   isInitialGame?: boolean;
@@ -9,12 +9,29 @@ interface SettingPageProps {
   onClick?: () => void;
 }
 
-export default function SettingsModal({isInitialGame = false, isStoppedGame = false, onClick }: SettingPageProps) {
+export default function SettingsModal({
+  isInitialGame = false,
+  isStoppedGame = false,
+  onClick,
+}: SettingPageProps) {
   const [musicValue, setMusicValue] = useState(50);
   const [volumeValue, setVolumeValue] = useState(50);
+
   return (
-    <div className="bg-[var(--white)] px-[4.5rem] py-12 rounded-4xl flex flex-col gap-4 items-center">
-      <p className="text-4xl text-[var(--primary)] font-orbitron">Configurações</p>
+    <Card
+      title="Configurações"
+      buttons={
+        <>
+          {isInitialGame && <Button text="Voltar" onClick={onClick} />}
+          {isStoppedGame && (
+            <div className="flex gap-4">
+              <Button text="Voltar ao jogo" onClick={onClick} className="px-1.5 py-2.5" />
+              <Button text="Sair" onClick={() => (window.location.href = "/menu")} />
+            </div>
+          )}
+        </>
+      }
+    >
       <div className="flex flex-col gap-4">
         <div className="flex flex-col items-center">
           <div className="flex items-center gap-4 mb-4">
@@ -31,7 +48,6 @@ export default function SettingsModal({isInitialGame = false, isStoppedGame = fa
               className="w-56 h-2 rounded-full appearance-none [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-[var(--primary)]"
             />
           </div>
-
           <div className="flex items-center gap-4">
             <Image src="/img/volume.svg" height={40} width={40} alt="Icone de volume" />
             <input
@@ -47,20 +63,7 @@ export default function SettingsModal({isInitialGame = false, isStoppedGame = fa
             />
           </div>
         </div>
-        <div className="flex justify-center">
-          {isInitialGame && <Button text="Voltar" onClick={onClick} />}
-          {isStoppedGame && (
-            <div className="flex gap-4">
-              <Button
-                text="Voltar ao jogo"
-                onClick={onClick}
-                className="px-1.5 py-2.5"
-              />
-              <Button text="Sair" onClick={() => (window.location.href = "/menu")} />
-            </div>
-          )}
-        </div>
       </div>
-    </div>
+    </Card>
   );
 }
