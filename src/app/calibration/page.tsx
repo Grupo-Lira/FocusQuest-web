fcada3af70b8021ca8c8eec21da9b1298bfe67c1
Amanda Oliveira <amandaol7b@gmail.com>
2025-10-10 01:09:43 -0300

    feat(calibration): enhance CalibrationPage with star interaction and modal handling

--- src/app/calibration/page.tsx
+++ src/app/calibration/page.tsx
@@ -1,41 +1,76 @@
 "use client";
 import NavbarCalibration from "@/components/Calibration/NavbarCalibration";
 import OverlayInstruction from "@/components/Calibration/OverlayInstruction";
-import Star from "@/components/Star";
-import { v4 as uuidv4 } from "uuid";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { stars } from "@/constants/calibrationStar";
+import StarCalibration from "@/components/Calibration/StarCalibration";
+import SettingsModal from "@/components/SettingsModal";
+import SuccessScreen from "@/components/Calibration/SuccessScreen";
 
 export default function CalibrationPage() {
   const [showInstructions, setShowInstructions] = useState(true);
+  const [successModalVisible, setSuccessModalVisible] = useState(true);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [hits, setHits] = useState(0);
 
-  const stars = [
-    { id: uuidv4(), top: 5, left: 3 },
-    { id: uuidv4(), top: 5, left: 50 },
-    { id: uuidv4(), top: 5, left: 95 },
-    { id: uuidv4(), top: 50, left: 3 },
-    { id: uuidv4(), top: 50, left: 95 },
-    { id: uuidv4(), top: 90, left: 3 },
-    { id: uuidv4(), top: 90, left: 50 },
-    { id: uuidv4(), top: 90, left: 95 },
-  ];
+  // Inicia o processo de calibração
+  const handleStartCalibration = () => {
+    setShowInstructions(false);
+  };
+
+  // Verifica quando o usuário completa a calibração
+  useEffect(() => {
+    if (hits === 5) {
+      setSuccessModalVisible(true);
+    }
+  }, [hits]);
+
+  // Reinicia calibração quando fechar o modal de sucesso
+  const handleRestart = () => {
+    setHits(0);
+    setSuccessModalVisible(false);
+    setShowInstructions(true);
+  };
 
   return (
-    <div className="min-h-screen flex flex-col">
-      {showInstructions && (
-        <OverlayInstruction onComplete={() => setShowInstructions(false)} />
-      )}
-      <NavbarCalibration />
-      <div className="flex-1 relative">
-        {stars.map((star) => (
-          <Star
-            key={star.id}
-            top={star.top}
-            left={star.left}
-            onError={() => console.error("Error with star", star.id)}
-            onRemove={() => console.log("Star removed", star.id)}
-          />
-        ))}
-      </div>
-    </div>
+    <>
+      {!isModalVisible ? (
+        <div className="min-h-screen flex flex-col text-white">
+            {/* Tela de instruções antes da calibração */}
+            {showInstructions && <OverlayInstruction onComplete={handleStartCalibration} />}
+
+            {/* Navbar de controle */}
+            <NavbarCalibration setIsModalOpen={() => setIsModalVisible(true)} />
+
+            {/* Área principal da calibração */}
+            <div className="flex-1 relative overflow-hidden">
+            {stars.map((star) => (
+                <StarCalibration
+                key={star.id}
+                top={star.top}
+                left={star.left}
+                onHit={() => setHits((prev) => prev + 1)}
+                onError={() => console.error("Erro com estrela", star.id)}
+                />
+            ))}
+            </div>
+
+            {/* Modal de sucesso */}
+            {successModalVisible && (
+                <SuccessScreen onRestart={handleRestart} />
+            )}
+        </div>
+      ): (
+        <div className="flex items-center justify-center min-h-screen">
+            <SettingsModal
+            isStoppedGame={true}
+            onClick={() => {
+                setIsModalVisible(false);
+            }}
+            />
+        </div>
+        )
+    }
+    </>
   );
 }
