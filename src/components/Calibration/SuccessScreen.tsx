d78dd1bfe01770ec191aa6c6ada1c7c0294071b8
Amanda Oliveira <amandaol7b@gmail.com>
2025-10-10 01:09:19 -0300

    feat: add SuccessScreen component for calibration completion

---
+++ src/components/Calibration/SuccessScreen.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import { Button } from "../Button";
+import { Card } from "../Card";
+
+type SuccessScreenProps = {
+  onRestart: () => void;
+};
+
+export default function SuccessScreen({onRestart}: SuccessScreenProps) {
+  return (
+    <div className="absolute inset-0 z-50 bg-black/70 flex items-center justify-center">
+        <Card
+        title={"Missão Cumprida!"}
+        buttons={
+            <div className="flex gap-4">
+            <Button text="Reiniciar" onClick={onRestart} />
+            <Button text="Finalizar" onClick={() => (window.location.href = "/menu")} />
+            </div>
+        }
+        >
+        <div className="flex flex-col gap-4">
+            <div className="flex flex-col items-center">
+            <Image
+                src="/img/successCalibration.svg"
+                height={400}
+                width={275}
+                alt="Personagem de missao cumprida"
+            />
+            </div>
+        </div>
+        </Card>
+    </div>
+  );
+}
