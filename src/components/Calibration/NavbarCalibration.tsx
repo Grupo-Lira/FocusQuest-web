e41c857bf927532376223b4c63daa5f73cccea8b
Amanda Oliveira <amandaol7b@gmail.com>
2025-10-10 01:09:40 -0300

    feat(calibration): add NavbarCalibration component with modal toggle functionality

--- src/components/Calibration/NavbarCalibration.tsx
+++ src/components/Calibration/NavbarCalibration.tsx
@@ -1,4 +1,10 @@
-export default function NavbarCalibration() {
+import { Bolt } from "lucide-react";
+
+type NavbarCalibrationProps = {
+  setIsModalOpen: (isOpen: boolean) => void;
+};
+
+export default function NavbarCalibration({ setIsModalOpen }: NavbarCalibrationProps) {
   return (
     <div className="pt-6 flex justify-center">
       <div className="bg-[var(--white)] px-6 py-3 flex w-fit rounded-full gap-40 h-[70px]">
@@ -7,6 +13,14 @@ export default function NavbarCalibration() {
             PRESSIONE CADA ESTRELA 5 VEZES ENQUANTO OLHA PARA A ESTRELA
           </p>
         </div>
+      </div>
+      <div
+        className="bg-[var(--primary)] z-20 w-11 h-11 rounded-full absolute flex items-center justify-center button-glow transition-all duration-300 top-9 right-9"
+        onClick={() => {
+          setIsModalOpen(true);
+        }}
+      >
+        <Bolt color="white" />
       </div>
     </div>
   );
